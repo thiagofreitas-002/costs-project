42a3ab6366f86952403844c1e5f0f1eea48c53cb
Thiago Freitas <thiagofreitas2909@gmail.com>
2022-08-07 12:13:31 -0300

    �  avançando

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,7 +1,20 @@
+import { useLocation } from "react-router-dom";
+
+import Message from "../layout/Message";
+
+import styles from "./Projects.module.css";
+
 function Projects() {
+  const location = useLocation();
+  let message = "";
+  if (location.state) {
+    message = location.state.message;
+  }
+
   return (
-    <div>
-      <h1>Projects</h1>
+    <div className={styles.test}>
+      <h1>Meus projetos</h1>
+      {message && <Message type="success" msg={message} />}
     </div>
   );
 }
