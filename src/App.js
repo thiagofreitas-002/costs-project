4cf1f58fc59bac4ea6f07a1297736c9be3128f8f
Thiago Freitas <thiagofreitas2909@gmail.com>
2022-08-07 17:32:51 -0300

    Diversas adições

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
+import Project from "./components/pages/Project";
 import Projects from "./components/pages/Projects";
 
 /* LAYOUT */
@@ -23,6 +24,7 @@ function App() {
           <Route path="/company" element={<Company />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/newproject" element={<NewProject />} />
+          <Route path="/project/:id" element={<Project />} />
         </Routes>
       </Container>
       <Footer />
--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,8 +1,105 @@
+import styles from "./Project.module.css";
+
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import Loading from "../layout/Loading"; /* Possivel erro */
+import Container from "../layout/Container";
+
+import ProjectForm from "../project/ProjectForm";
+
 function Project() {
+  const { id } = useParams();
+
+  const [project, setProject] = useState({});
+  const [showProjectForm, setShowProjectForm] = useState(false);
+  const [message, setMessage] = useState();
+  const [type, setType] = useState();
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(`http://localhost:5000/projects/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((resp) => resp.json())
+        .then((data) => {
+          setProject(data);
+        })
+        .catch((err) => console.log(err));
+    }, 300);
+  }, [id]);
+
+  function editPost(project) {
+    // budget validation
+
+    if (project.budget < project.cost) {
+      //message
+    }
+
+    fetch(`http://localhost:5000/projects/${project.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProject(data);
+        setShowProjectForm(false);
+        //message
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function toggleProjectForm() {
+    setShowProjectForm(!showProjectForm);
+  }
+
   return (
-    <div>
-      <h1>Project</h1>
-    </div>
+    <>
+      {project.name ? (
+        <div className={styles.project_details}>
+          <Container customClass="column">
+            <div className={styles.details_container}>
+              {/* <div className={styles.project_title_container}> */}
+              <h1>Projeto: {project.name}</h1>
+              <button className={styles.btn} onClick={toggleProjectForm}>
+                {!showProjectForm ? "Editar Projeto" : "Fechar"}
+              </button>
+              {/* </div> */}
+              {!showProjectForm ? (
+                <div className={styles.project_info}>
+                  <p>
+                    <span>Categoria: </span> {project.category.name}
+                  </p>
+                  <p>
+                    <span>Total do Orçamento: </span> R${project.budget}
+                  </p>
+                  <p>
+                    <span>Total Utilizado: </span> R${project.cost}
+                  </p>
+                </div>
+              ) : (
+                <div className={styles.project_info}>
+                  <ProjectForm
+                    handleSubmit={editPost}
+                    btnText="Concluir edição"
+                    projectData={project}
+                  />
+                </div>
+              )}
+            </div>
+          </Container>
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
   );
 }
 
